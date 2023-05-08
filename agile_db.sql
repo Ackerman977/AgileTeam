32f96f145078aa7ebe05a7c918602ad3dcddc243
ManuelaAlgenii <83863824+ManuelaAlgenii@users.noreply.github.com>
2023-05-08 10:18:20 +0200

    Update agile_db.sql

--- agile_db.sql
+++ agile_db.sql
@@ -7,6 +7,11 @@
 -- Versione del server: 10.4.28-MariaDB
 -- Versione PHP: 8.2.4
 
+
+create database if not exists agile_db;
+use agile_db;
+
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
