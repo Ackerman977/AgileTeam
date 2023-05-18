ab1974b17fc2ef902da831da333e8d2edecb85fb
Martoz007 <110594680+Martoz007@users.noreply.github.com>
2023-05-18 10:03:44 +0200

    aggiornamento totale cartella

---
+++ ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testSum()
+    {
+        $result = 2 + 2;
+        $this->assertEquals(4, $result);
+    }
+
+    public function testStringConcatenation()
+    {
+        $result = "Hello" . " " . "World";
+        $this->assertEquals("Hello World", $result);
+    }
+}
