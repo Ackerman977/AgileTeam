49988985d29016d25f2e74b10ebe8385b78f796f
Asja <asja.valentini@student.univaq.it>
2023-05-24 14:40:26 +0200

    Aggiornamento test 1

--- ExampleTest1.php
+++ ExampleTest1.php
@@ -1,69 +1,37 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest1 extends TestCase
+class UserTest extends TestCase
 {
-    public function testRegistrationValidation()
+    public function testLogin()
     {
-        $existingUsers = ['user1']; // Array di utenti esistenti
-        $nome = 'john';
-        $email = 'john.doe@example.com';
-        $username = 'johndoe';
-        $password = 'password123';
+        $user = [
+            'username' => 'testuser',
+            'password' => password_hash('testpassword', PASSWORD_DEFAULT)
+        ];
 
-        // Verifica se il messaggio è corretto quando ci sono utenti esistenti
-        $this->assertTrue($this->validateRegistration($existingUsers, $nome, $email, $username, $password, 'Name, email, or username already in use: %s'));
+        // Simula la richiesta POST con le credenziali dell'utente
+        $password = 'testpassword';
 
-        // Verifica se il messaggio è corretto quando il campo "Nome" è vuoto
-        $this->assertEquals('The "Nome" field is required.', $this->validateRegistration($existingUsers, '', $email, $username, $password, 'The "Nome" field is required.'));
-        // Verifica se il messaggio è corretto quando l'email non è valida
-        $this->assertFalse($this->validateRegistration($existingUsers, $nome, 'invalid_email', $username, $password, 'Please enter a valid email address: %s'));
+        // Esegui la funzione da testare
+        $result = $this->login($user, $password);
 
-        // Verifica se il messaggio è corretto quando lo username non è valido
-        $this->assertTrue($this->validateRegistration($existingUsers, $nome, $email, 'invalid_username', $password, 'The username is not valid. Only alphanumeric characters and underscore are allowed. Minimum length: 3 characters. Maximum length: 20 characters: %s'));
-
-        // Verifica se il messaggio è corretto quando la lunghezza della password non è valida
-        $this->assertFalse($this->validateRegistration($existingUsers, $nome, $email, $username, 'short', 'Password length should be between 8 and 20 characters: %s'));
-
-        // Verifica se il messaggio è corretto quando tutti i dati sono validi
-        $this->assertTrue($this->validateRegistration($existingUsers, $nome, $email, $username, $password, '')); // Non dovrebbe esserci alcun messaggio di errore
+        // Verifica che la funzione restituisca true
+        $this->assertTrue($result);
     }
 
-    public function validateRegistration($existingUsers, $nome, $email, $username, $password, $expectedErrorMessage)
+    private function login($user, $password)
     {
-        $msg = '';
-    
-        if (in_array($username, $existingUsers)) {
-            $msg = $expectedErrorMessage;
-            return false;
-        } elseif (trim($nome) === '') {
-            $msg = $expectedErrorMessage;
-            return $msg;
-        } elseif (empty($email)) {
-            $msg = sprintf($expectedErrorMessage, $email);
-            return false;
-        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $msg = sprintf($expectedErrorMessage, $email);
-            return false;
-        } elseif (!preg_match("/^[a-z\d_]{3,20}$/i", $username)) {
-            $msg = sprintf($expectedErrorMessage, $username);
-            return false;
-        } elseif (strlen($password) < 8 || strlen($password) > 20) {
-            $msg = sprintf($expectedErrorMessage, $password);
-            return false;
+        if (!$user || password_verify($password, $user['password']) === false) {
+            $msg = 'Credenziali utente errate %s';
+        } else {
+            // rigenera l'ID di sessione e salva le informazioni dell'utente nella sessione
+            // ...
+
+            // reindirizza l'utente alla dashboard
+            // ...
         }
-    
-        // Output debug information
-        echo "Existing Users: " . implode(', ', $existingUsers) . PHP_EOL;
-        echo "Username: " . $username . PHP_EOL;
-        echo "Nome: " . $nome . PHP_EOL;
-        echo "Email: " . $email . PHP_EOL;
-        echo "Password: " . $password . PHP_EOL;
-        
-        // Verifica se il messaggio di errore non è vuoto
-        $this->assertEmpty($msg, $expectedErrorMessage);
-    
-        return $msg === '';
+
+        return true;
     }
-    
 }
